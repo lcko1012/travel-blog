da485c31f47c4c9612ff77cd6503d6a5aa50d91d
lcko1012 <lecanhkieuoanh@gmail.com>
2021-05-18 17:30:50 +0700

    fix refresh token

--- src/App.js
+++ src/App.js
@@ -6,33 +6,63 @@ import Header from './users/header/Header'
 import Body from './users/Body';
 import Footer from './users/footer/Footer';
 import { useDispatch, useSelector } from 'react-redux';
-import { dispatchLogin } from './redux/actions/authAction'
+import { dispatchGetUser, dispatchLogin, fetchUser } from './redux/actions/authAction'
 import Cookies, { get } from 'js-cookie';
 import { useState } from 'react';
 import jwt_decode from 'jwt-decode'
 
 function App() {
   const dispatch = useDispatch()
-  const token = Cookies.get('token')
-  console.log(token)
+  const auth = useSelector(state => state.auth)
 
 
   useEffect(() => {
-    console.log("app")
-    if(token){
+    if (localStorage.getItem("firstLogin")) {
+      console.log("check first login")
+
+      const getToken = async () => {
+        console.log(token)
+        const res = await axios.post("/auth/refreshToken", null, {
+          headers: { Authorization: `Bearer ${token}` }
+        })
+        Cookies.set("token", res.data.token)
+        Cookies.set("duration", res.data.duration)
+      }
+
+      const token = Cookies.get("token")
+      const duration = Cookies.get("duration")
+          fetchUser(token).then(res => {
+            dispatch(dispatchGetUser(res))
+          })
+
+      if (token && duration) {
+        const interval = setInterval(() => {
+          console.log("get refresh token")
+          getToken()
+        }, 180000)
+        return () => clearInterval(interval)
+      }
+    }
+  }, [auth.isLogged, dispatch])
+
+  useEffect(() => {
+    const token = Cookies.get("token")
+    if (token) {
+      console.log("zo app check token")
       const { exp } = jwt_decode(token)
       const expirationTime = (exp * 1000) - 60000
       if (Date.now() < expirationTime) {
         const getToken = async () => {
-                console.log("goi refresh trong app")
-                const res = await axios.post("/auth/refreshToken", null, {
-                    headers: { Authorization: `Bearer ${token}` }
-                })
-                Cookies.set("token", res.data.token)
-                Cookies.set("duration", res.data.duration)
+          console.log("goi refresh trong app")
+          const res = await axios.post("/auth/refreshToken", null, {
+            headers: { Authorization: `Bearer ${token}` }
+          })
+          Cookies.set("token", res.data.token)
+          Cookies.set("duration", res.data.duration)
         }
         const getUser = () => {
           dispatch(dispatchLogin())
+          // return 
         }
         getToken()
         getUser()
@@ -43,8 +73,8 @@ function App() {
         localStorage.removeItem("firstLogin")
       }
     }
-   
-  }, [token])
+
+  }, [])
 
   return (
     <Router>

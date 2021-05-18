a65963259248e685a501d1ab857d92f3ec5a4c55
lcko1012 <lecanhkieuoanh@gmail.com>
2021-05-18 17:31:28 +0700

    get user infor

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -10,6 +10,31 @@ export const dispatchLogin = () => {
 }
 
 //TODO: GET USER INFOR
-// export const fetchUser = async (token) => {
+export const fetchUser = async (token) => {
+    const res = await axios.get('/whoami', {
+        headers: {Authorization: `Bearer ${token}`}
+    })
+    return res
+}
 
-// }
+export const dispatchGetUser = (res) => {
+    if(res.data.role === "ROLE_ADMIN"){
+        return {
+            type: ACTIONS.GET_USER,
+            payload: {
+                user: res.data,
+                isAdmin: true
+            }
+        }
+    }
+    else {
+        return {
+            type: ACTIONS.GET_USER, 
+            payload: {
+                user: res.data,
+                isAdmin: false
+            }
+        }
+    }
+    
+}

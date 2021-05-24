7b8601bd16f4fbd448458ea202a744eb0226e8cf
lcko1012 <lecanhkieuoanh@gmail.com>
2021-05-24 19:49:55 +0700

    updating loading page for Bookmark and search

--- src/users/body/bookmark/Bookmarks.js
+++ src/users/body/bookmark/Bookmarks.js
@@ -2,22 +2,33 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 import React, { useEffect, useState } from 'react'
 import Empty from '../../utils/Empty/Empty'
+import Loading from '../../utils/Loading/Loading'
 import CurrentPost from '../home/components/CurrentPost'
 
 const Bookmarks = () => {
     const [listBookmarks, setListBookmarks] = useState([])
+    const [currentPage, setCurrentPage] = useState(0)
+    const [isEmpty, setIsEmpty] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+
+
     useEffect(() => {
         const token = Cookies.get("token")
         const getListBookmarks = async () => {
             try {
+                console.log(currentPage)
                 if (token) {
                     const res = await axios.get('/bookmark/posts', {
+                        params: { page: currentPage },
                         headers: { Authorization: `Bearer ${token}` }
                     })
                     if (res) {
                         console.log(res)
-                        setListBookmarks(res.data)
-                        console.log(listBookmarks)
+                        setListBookmarks([...listBookmarks, ...res.data])
+                        if (res.data.length === 0) {
+                            setIsEmpty(true)
+                        }
+                        setIsLoading(false)
                     }
                 }
             } catch (err) {
@@ -27,20 +38,39 @@ const Bookmarks = () => {
 
         getListBookmarks()
 
-    }, [])
+    }, [currentPage])
     console.log(listBookmarks)
     return (
         <main className="main__home">
             <div className="container pb-50 pt-30">
-                <div className="font-small text-uppercase pb-15">
-                    <h5 style={{ fontSize: '14px' }}>Các bài viết bạn đã bookmark:</h5>
-                </div>
-                {listBookmarks.length === 0 ? <Empty /> :
-                    listBookmarks.map((post, index) => {
-                        return (
-                            <CurrentPost post={post} key={index} />
-                        )
-                    })}
+                {!isLoading ?
+                    <>
+                        <div className="font-small text-uppercase pb-15">
+                            <h5 style={{ fontSize: '14px' }}>Các bài viết bạn đã bookmark:</h5>
+                        </div>
+                        {listBookmarks.length === 0 ? <Empty /> :
+                            listBookmarks.map((post, index) => {
+                                return (
+                                    <CurrentPost post={post} key={index} />
+                                )
+                            })}
+
+                        <div className="pagination-area mb-30">
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination justify-content-start">
+                                    <li className={`page-item" ${isEmpty ? 'disabled' : null}`}
+                                        onClick={() => setCurrentPage(currentPage + 1)}>
+                                        <a className="page-link">
+                                            <i className="fal fa-long-arrow-right"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </>
+                    :
+                    <Loading />
+                }
             </div>
         </main>
     )
--- src/users/body/search/SearchPage.js
+++ src/users/body/search/SearchPage.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom'
 import Empty from '../../utils/Empty/Empty';
+import Loading from '../../utils/Loading/Loading';
 import CurrentPost from '../home/components/CurrentPost';
 
 const SearchPage = () => {
@@ -14,6 +15,8 @@ const SearchPage = () => {
     const [postsResult, setPostsResult] = useState([])
     //Kiem tra trang tiep theo co rong k?
     const [isEmpty, setIsEmpty] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+
 
     useEffect(() => {
         try {
@@ -28,9 +31,10 @@ const SearchPage = () => {
                 if (res) {
                     setPostsResult([...postsResult, ...res.data])
                     console.log(res.data.length)
-                    if(res.data.length === 0) {
+                    if (res.data.length === 0) {
                         setIsEmpty(true)
                     }
+                    setIsLoading(false)
                 }
             }
             findPost()
@@ -38,30 +42,37 @@ const SearchPage = () => {
             console.log(error)
         }
     }, [currentPage])
+
     return (
         <main className="main__home">
             <div className="container pb-50 pt-30">
-                <div className="font-small text-uppercase pb-15">
-                    <h5 style={{ fontSize: '14px' }}>Kết quả tìm kiếm:</h5>
-                </div>
-                {postsResult.length === 0 ? <Empty /> :
-                    postsResult.map((post, index) => {
-                        return (
-                            <CurrentPost post={post} key={index} />
-                        )
-                })}
-                <div className="pagination-area mb-30">
-                    <nav aria-label="Page navigation example">
-                        <ul className="pagination justify-content-start">
-                            <li className={`page-item" ${isEmpty ? 'disabled' : null}`}
-                            onClick={() => setCurrentPage(currentPage + 1)}>
-                                <a className="page-link">
-                                    <i className="fal fa-long-arrow-right"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+                {!isLoading ?
+                    <>
+                        <div className="font-small text-uppercase pb-15">
+                            <h5 style={{ fontSize: '14px' }}>Kết quả tìm kiếm:</h5>
+                        </div>
+                        {postsResult.length === 0 ? <Empty /> :
+                            postsResult.map((post, index) => {
+                                return (
+                                    <CurrentPost post={post} key={index} />
+                                )
+                            })}
+                        <div className="pagination-area mb-30">
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination justify-content-start">
+                                    <li className={`page-item" ${isEmpty ? 'disabled' : null}`}
+                                        onClick={() => setCurrentPage(currentPage + 1)}>
+                                        <a className="page-link">
+                                            <i className="fal fa-long-arrow-right"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </>
+                    :
+                    <Loading />
+                }
             </div>
         </main>
     )
